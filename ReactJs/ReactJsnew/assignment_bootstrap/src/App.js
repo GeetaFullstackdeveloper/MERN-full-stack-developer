5eb392d93f156407cc09daae32a8da0752faee05
Geeta1987 <d@com>
2024-12-27 07:24:37 +0530

    bootstrap_project_1

---
+++ ReactJs/ReactJsnew/assignment_bootstrap/src/App.js
@@ -0,0 +1,13 @@
+import ContactUs from "./pages/Contanctus";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <ContactUs />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJs/ReactJsnew/assignment_bootstrap/src/components/mainnavbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// type rafce to get components
+
+const MainNavbar = () => {
+  return (
+    <div>
+        <h1>mainnavbar</h1>
+        </div>
+  )
+}
+
+export default MainNavbar
